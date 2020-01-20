8089ff98e086dcd3e12134a6e92beec2b9de7b4c
Mihai <mihai4ever95@yahoo.com>
2020-01-21 01:16:54 +0200

    hello

---
+++ src/JunitTest.java
@@ -0,0 +1,10 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        SuiteTest1.class,
+        SuiteTest2.class,
+})
+public class JunitTest {
+}
---
+++ src/SuiteTest1.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SuiteTest1 {
+
+    public String message = "Saurabh";
+
+    JUnitMessage junitMessage = new JUnitMessage(message);
+
+    @Test(expected = ArithmeticException.class)
+    public void testJUnitMessage() {
+
+        System.out.println("Junit Message is printing ");
+        junitMessage.printMessage();
+
+    }
+
+    @Test
+    public void testJUnitHiMessage() {
+        message = "Hi!" + message;
+        System.out.println("Junit Hi Message is printing ");
+        assertEquals(message, junitMessage.salutationMessage());
+        System.out.println("Suite Test 2 is successful " + message);
+    }
+}
